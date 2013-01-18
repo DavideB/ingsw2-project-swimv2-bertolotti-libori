b2eaef1241c1660a84f054672cdfc56fb2d74f22
Emanuele <emanuele.libori@gmail.com>
2013-01-18 19:45:49 +0100

    Migliorato l'upload dell'immagine

---
+++ swim2Web/src/it/polimi/swim2/frontend/SendFriendshipRequest.java
@@ -0,0 +1,64 @@
+package it.polimi.swim2.frontend;
+
+import it.polimi.swim2.interfaces.StatelessEJB;
+import it.polimi.swim2.interfaces.StatelessFriendshipBean;
+import it.polimi.swim2.persistence.Friendshiprequest;
+import it.polimi.swim2.persistence.Registered;
+
+import java.io.IOException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SendFriendshipRequest
+ */
+public class SendFriendshipRequest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private StatelessEJB statelessBean;
+	private StatelessFriendshipBean friendship;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SendFriendshipRequest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public void init(ServletConfig config) throws ServletException {
+		super.init();
+		try {
+	    	
+	  	      Context context = new InitialContext();
+	  	      statelessBean = (StatelessEJB) context.lookup("swim2/UserManager/remote");
+	  	      friendship = (StatelessFriendshipBean) context.lookup("swim2/FriendshipManager/remote");
+	  	    } catch (NamingException e) {
+	  	      e.printStackTrace();
+	  	    }
+	}
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String target = request.getParameter("target");
+		String message = request.getParameter("message");
+		Registered r = statelessBean.getUserData(target);
+		friendship.addFriend(target, statelessBean.getUserData((String)request.getSession().getAttribute("username")).getId(), message);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
